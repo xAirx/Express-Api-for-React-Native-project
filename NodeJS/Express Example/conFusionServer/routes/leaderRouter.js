c7134f5de31161a90744ba48e9998c7358321b02
xAirx <marcowurtz@hotmail.com>
2020-02-05 13:48:26 +0100

    reverted to basic authentication, lack of documentation, lesson learned..didnt touch a project for 3 months..

---
+++ NodeJS/Express Example/conFusionServer/routes/leaderRouter.js
@@ -0,0 +1,223 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const Leaders = require('../models/leaders');
+
+const LeaderRouter = express.Router();
+
+LeaderRouter.use(bodyParser.json());
+
+LeaderRouter.route('/')
+    .get((req, res, next) => {
+        Leaders.find({})
+            .then((leaders) => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(leaders);
+            }, (err) => next(err))
+            .catch((err) => next(err));
+    })
+    .post((req, res, next) => {
+        Leaders.create(req.body)
+            .then((leader) => {
+                console.log('Leader Created ', leaders);
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(leaders);
+            }, (err) => next(err))
+            .catch((err) => next(err));
+    })
+    .put((req, res, next) => {
+        res.statusCode = 403;
+        res.end('PUT operation not supported on /leaders');
+    })
+    .delete((req, res, next) => {
+        Leaders.remove({})
+            .then((resp) => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(resp);
+            }, (err) => next(err))
+            .catch((err) => next(err));
+    });
+
+LeaderRouter.route('/:leaderId')
+    .get((req, res, next) => {
+        Leaders.findById(req.params.leaderId)
+            .then((leaders) => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(leaders);
+            }, (err) => next(err))
+            .catch((err) => next(err));
+    })
+    .post((req, res, next) => {
+        res.statusCode = 403;
+        res.end('POST operation not supported on /leaders/' + req.params.leaderId);
+    })
+    .put((req, res, next) => {
+        Leaders.findByIdAndUpdate(req.params.leaderId, {
+            $set: req.body
+        }, { new: true })
+            .then((leaders) => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(leaders);
+            }, (err) => next(err))
+            .catch((err) => next(err));
+    })
+    .delete((req, res, next) => {
+        Leaders.findByIdAndRemove(req.params.leaderId)
+            .then((resp) => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(resp);
+            }, (err) => next(err))
+            .catch((err) => next(err));
+    });
+
+//// DISH ID feedbackS
+
+LeaderRouter.route('/:leaderId/feedback')
+    .get((req, res, next) => {
+        Leaders.findById(req.params.leaderId)
+            .then((leader) => {
+                if (leader != null) {
+                    res.statusCode = 200;
+                    res.setHeader('Content-Type', 'application/json');
+                    res.json(leaders.feedbacks);
+                }
+                else {
+                    err = new Error('Leader ' + req.params.leaderId + ' not found');
+                    err.status = 404;
+                    return next(err);
+                }
+            }, (err) => next(err))
+            .catch((err) => next(err));
+    })
+    .post((req, res, next) => {
+        Leaders.findByIdAndUpdate(req.params.leaderId,
+            { "$push": { "feedback": req.body } },
+            { "new": true },
+            function (err, managerparent) {
+                if (err) throw err;
+                res.statusCode = 200;
+                res.send('Added feedback to successfully')
+                console.log(managerparent);
+            },
+            // Send Success RESPONSE.
+        );
+    })
+    .put((req, res, next) => {
+        res.statusCode = 403;
+        res.end('PUT operation not supported on /leaders/'
+            + req.params.leaderId + '/feedbacks');
+    })
+    .delete((req, res, next) => {
+        Leaders.findById(req.params.leaderId)
+            .then((leader) => {
+                if (leader != null) {
+                    for (var i = (leader.feedbacks.length - 1); i >= 0; i--) {
+                        leader.feedbacks.id(leader.feedbacks[i]._id).remove();
+                    }
+                    leader.save()
+                        .then((leader) => {
+                            res.statusCode = 200;
+                            res.setHeader('Content-Type', 'application/json');
+                            res.json(leader);
+                        }, (err) => next(err));
+                }
+                else {
+                    err = new Error('Leader ' + req.params.leaderId + ' not found');
+                    err.status = 404;
+                    return next(err);
+                }
+            }, (err) => next(err))
+            .catch((err) => next(err));
+    });
+
+LeaderRouter.route('/:leaderId/feedback/:feedbackId')
+    .get((req, res, next) => {
+        Leaders.findById(req.params.leaderId)
+            .then((leader) => {
+                if (leader != null && leader.feedbacks.id(req.params.feedbackId) != null) {
+                    res.statusCode = 200;
+                    res.setHeader('Content-Type', 'application/json');
+                    res.json(leader.feedbacks.id(req.params.feedbackId));
+                }
+                else if (leader == null) {
+                    err = new Error('Leader ' + req.params.leaderId + ' not found');
+                    err.status = 404;
+                    return next(err);
+                }
+                else {
+                    err = new Error('feedback ' + req.params.feedbackId + ' not found');
+                    err.status = 404;
+                    return next(err);
+                }
+            }, (err) => next(err))
+            .catch((err) => next(err));
+    })
+    .post((req, res, next) => {
+        res.statusCode = 403;
+        res.end('POST operation not supported on /leaders/' + req.params.leaderId
+            + '/feedbacks/' + req.params.feedbackId);
+    })
+    .put((req, res, next) => {
+        Leaders.findById(req.params.leaderId)
+            .then((leaders) => {
+                if (leaders != null && leaders.feedbacks.id(req.params.feedbackId) != null) {
+                    if (req.body.rating) {
+                        leaders.feedbacks.id(req.params.feedbackId).rating = req.body.rating;
+                    }
+                    if (req.body.feedback) {
+                        leaders.feedbacks.id(req.params.feedbackId).feedback = req.body.feedback;
+                    }
+                    leader.save()
+                        .then((leader) => {
+                            res.statusCode = 200;
+                            res.setHeader('Content-Type', 'application/json');
+                            res.json(leaders);
+                        }, (err) => next(err));
+                }
+                else if (leaders == null) {
+                    err = new Error('Leader ' + req.params.leaderId + ' not found');
+                    err.status = 404;
+                    return next(err);
+                }
+                else {
+                    err = new Error('feedback ' + req.params.feedbackId + ' not found');
+                    err.status = 404;
+                    return next(err);
+                }
+            }, (err) => next(err))
+            .catch((err) => next(err));
+    })
+    .delete((req, res, next) => {
+        Leaders.findById(req.params.leaderId)
+            .then((leaders) => {
+                if (leaders != null && leaders.feedbacks.id(req.params.feedbackId) != null) {
+                    leaders.feedbacks.id(req.params.feedbackId).remove();
+                    leaders.save()
+                        .then((leaders) => {
+                            res.statusCode = 200;
+                            res.setHeader('Content-Type', 'application/json');
+                            res.json(leaders);
+                        }, (err) => next(err));
+                }
+                else if (leaders == null) {
+                    err = new Error('Leaders ' + req.params.leaderId + ' not found');
+                    err.status = 404;
+                    return next(err);
+                }
+                else {
+                    err = new Error('feedback ' + req.params.feedbackId + ' not found');
+                    err.status = 404;
+                    return next(err);
+                }
+            }, (err) => next(err))
+            .catch((err) => next(err));
+    });
+
+module.exports = LeaderRouter;
